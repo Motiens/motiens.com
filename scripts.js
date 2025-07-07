bbc47f69fa4d70c8f58be1a2b03aabd4dab13169
Motiens <Motiens.Company@gmail.com>
2025-07-07 17:27:05 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,1 +1,6 @@
-// Funciones JS MOTIENS
+// Mostrar pop-up de test después de 10 segundos
+window.addEventListener('DOMContentLoaded', () => {
+  setTimeout(() => {
+    alert('¿Qué servicio es ideal para tu equipo? (Este será un pop-up real pronto)');
+  }, 10000);
+});
