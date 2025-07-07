55c1a91137400b9622b309418b36eed19f53c166
Motiens <Motiens.Company@gmail.com>
2025-07-07 09:56:55 +0200

    Add files via upload

--- script.js
+++ script.js
@@ -1,14 +1,2 @@
-window.addEventListener('scroll', () => {
-    const popup = document.getElementById('popup');
-    if (window.scrollY > 300) {
-        popup.style.display = 'block';
-    }
-});
 
-function cerrarPopup() {
-    document.getElementById('popup').style.display = 'none';
-}
-
-function mostrarFormulario() {
-    document.getElementById('popup').style.display = 'block';
-}
+console.log("MOTIENS sitio cargado");
